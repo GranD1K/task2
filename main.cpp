7184bf2b8e3a81df9417ded7319894c953d3f512
Maksim Leontev <akagrand1k@yandex.ru>
2016-12-25 16:49:28 +0300

    Added sourse and header files in project

--- main.cpp
+++ main.cpp
@@ -1,29 +1,7 @@
 #include <iostream>
+#include "oop.h"
 
 using namespace std;
-
-class Base
-{
-public:
-    Base() { cout << "Construct Base object\n"; }
-    ~Base() { cout << "Destruct Base object\n"; }    
-    virtual void func() { cout << "Function func()\ Create Maksim Leontev group 321 knit 2016 December\n"; }
-};
-
-class ChildClass : public Base
-{
-    public :
-    ChildClass() { cout << "Construct Child object\n";}
-    ~ChildClass() { cout << "Destruct Child object\n";}
-    
-    void func() { cout << "Function func() of class Child :)\n"; }    
-};
-
-void Function(const Base &obj)
-{
-    cout << "*More  Fun with function:) for object obj" << endl;
-    obj.func();
-}
 
 int main(int argc, char *argv[])
 {    
